c4ff7c5e0dcef9e1f14993b1da5261d799ff5deb
Dinh Van Nguyen <e1800935@edu.vamk.fi>
2019-04-02 09:28:15 +0100

    modified main.c for recording

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
+#include <stdlib.h>		// for random numbers
+#include <time.h>		// for randomization (using system time)
+#include "screen.h"
 #include <stdio.h>
-int main() {
-	foo();
-	printf("message from main\");
-}
+#include "sound.h"
 
+int main(){
+	FILE *f;
+	short sd[80000];
+	for(;;){
+		system(RCMD);
+		f = fopen("test.wav", "r");
+		if(f == NULL){
+			printf("Cannot open the file\n");
+			return 1;
+		}
+
+		clearScreen();
+		setColors(RED, bg(YELLOW));
+		struct WAVHDR hdr;
+		fread(&hdr, sizeof(hdr), 1, f);		// read WAV header
+		fread(&sd, sizeof(sd), 1, f);	//read WAV data
+		fclose(f);
+		displayWAVHDR(hdr);
+		//displayWAVDATA();
+	}
+	resetColors();
+	getchar();
+}
